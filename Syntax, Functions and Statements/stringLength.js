d9be9ab940b6f359cb4f171774244f96c2225592
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-05-15 15:19:03 +0300

    Create stringLength.js

---
+++ Syntax, Functions and Statements/stringLength.js
@@ -0,0 +1,8 @@
+function stringLength(string1, string2, string3)
+{
+   let sumLengths = string1.length + string2.length + string3.length;
+   let averageLength = Math.floor(sumLengths / 3);
+
+   console.log(sumLengths);
+   console.log(averageLength);
+}
