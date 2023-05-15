5e263e7646bb57904e45b84f2bde99c6e9d0b9e8
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-05-15 15:30:19 +0300

    Create mathOperations.js

---
+++ Syntax, Functions and Statements/mathOperations.js
@@ -0,0 +1,34 @@
+function mathOperations(num1, num2, operator)
+{
+    let result = 0;
+
+    switch(operator)
+    {
+        case '+':
+            result = num1 + num2;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            break;
+
+        case '/':
+            result = num1 / num2;
+            break;
+
+        case '%':
+            result = num1 % num2;
+            break;
+
+        case '**':
+            result = Math.pow(num1, num2);
+            break;
+    }
+
+
+    console.log(result);
+}
