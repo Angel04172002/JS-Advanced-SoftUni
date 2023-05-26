1a8c9acfedeeeee25a73986fdfaace1922b941d2
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-05-26 18:11:17 +0300

    Create extractIncreasingSubsetFromArray.js

---
+++ Exercise: Arrays and nested arrays/extractIncreasingSubsetFromArray.js
@@ -0,0 +1,19 @@
+function extractIncreasingSubsetFromArray(arr) {
+
+    let maxElement = Number.MIN_SAFE_INTEGER;
+
+    let result = arr.reduce((acc, curr) => {
+
+        if (curr > maxElement) {
+            maxElement = curr;
+            let temp = acc;
+            temp.push(curr);
+            return temp;
+        }
+
+        return acc;
+    }, []);
+
+    return result;
+}
+
