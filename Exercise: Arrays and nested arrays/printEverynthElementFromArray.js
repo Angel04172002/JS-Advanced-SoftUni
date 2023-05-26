1c47af1726117e088d83a374b8abfcf79c5f152e
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-05-26 17:43:40 +0300

    Create printEverynthElementFromArray.js

---
+++ Exercise: Arrays and nested arrays/printEverynthElementFromArray.js
@@ -0,0 +1,12 @@
+function printEverynthElementFromArray(arr, step)
+{
+    let output = []
+
+    for(let i = 0; i < arr.length; i += step)
+    {
+        output.push(arr[i])
+    }  
+    
+    return output
+}
+
