b4642ebc48e37db81866c89caebe7daba1c5be60
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-06-09 09:33:16 +0300

    Create rectangle.js

---
+++ Exercise: Objects and Composition/rectangle.js
@@ -0,0 +1,13 @@
+function rectangle(width, height, color) {
+
+    let rectangle = {
+        width,
+        height,
+        color: color[0].toUpperCase() + color.slice(1),
+        calcArea() {
+            return this.width * this.height;
+        },
+    };
+
+    return rectangle;
+}
