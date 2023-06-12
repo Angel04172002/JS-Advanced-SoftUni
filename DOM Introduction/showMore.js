97162ff0ac3645792b467b9d0c4004cdbeb2323d
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-06-12 13:07:29 +0300

    Create showMore.js

---
+++ DOM Introduction/showMore.js
@@ -0,0 +1,9 @@
+function showText() {
+    
+    let linkElement = document.getElementById('more');
+    let spanElement = document.getElementById('text');
+
+
+    linkElement.style.display = 'none';
+    spanElement.style.display = 'inline';
+}
